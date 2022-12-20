2b57931a853de24286bda13d5d9ae0e37c1f463e
Jelena Obradovic <jelena.obradovic@dilig.net>
2022-12-20 13:28:38 +0100

    Code formatting

--- cypress/fixtures/PageObject/TargetAccountPage.js
+++ cypress/fixtures/PageObject/TargetAccountPage.js
@@ -1,34 +1,33 @@
-class TargetAccount{
+class TargetAccount {
 
 
-    static getAccountInputAtSearch()
-    {
+    static getAccountInputAtSearch() {
         return cy.xpath("//input[@id='float-input']")
     }
 
-    static getSearchButton()
-    {
+    static getSearchButton() {
         return cy.xpath("//button[contains(text(),'Search')]")
     }
 
-    static getAccountTdsFromTable()
-    {
+    static getAccountTdsFromTable() {
         return cy.xpath("//tbody[@class='ng-star-inserted']/tr/td[1]")
     }
 
-    static getTargetButtons()
-    {
+    static getTargetButtons() {
         return cy.xpath("//tbody[@class='ng-star-inserted']/tr/td[8]")
     }
 
-    static getTargetAccountInput()
-    {
+    static getTargetAccountInput() {
         return cy.xpath('//tfoot//td[2]/input')
+    }
+
+    static getAddTargetAccountButton() {
+        return cy.xpath("//tfoot//td[5]//button")
     }
 
 }
 
-module.exports =TargetAccount
+module.exports = TargetAccount
 
 
 
